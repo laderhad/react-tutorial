59a4c2c9576b51619083dc15bef5d4fd598fefff
Utku Kerem <68512069+laderhad@users.noreply.github.com>
2025-02-08 00:05:34 +0300

    counter with useState

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,9 @@
-import MyComponent from './MyComponent.jsx';
+import Counter from "./Counter";
 
 function App() {
   
   return (
-    <MyComponent/>
+    <Counter/>
   );
   
 }
--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -25,3 +25,4 @@ function Counter(){
     );
 
 }
+export default Counter
