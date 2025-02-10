b2c7d88fc5e210c8b6d2d8159ab5060baefa8832
Utku Kerem <68512069+laderhad@users.noreply.github.com>
2025-02-10 21:25:57 +0300

    color picker app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
-import Counter from "./Counter";
-import MyComponent from "./MyComponent";
+import ColorPicker from "./ColorPicker";
+
 
 function App() {
   
   return (
-    <MyComponent/>
+    <ColorPicker/>
   );
   
 }
